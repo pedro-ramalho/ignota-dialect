9d8241c14a5920138e76efbd0040bf891c482b35
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2024-04-18 09:30:31 +0100

    feat: registered upstream dialects (Arith, Func, Math)

--- src/ignota-opt.cpp
+++ src/ignota-opt.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <mlir/Dialect/Arith/IR/Arith.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/Dialect/Math/IR/Math.h>
 #include <mlir/IR/DialectRegistry.h>
+#include <mlir/InitAllDialects.h>
 #include <mlir/Tools/mlir-opt/MlirOptMain.h>
 
 int main(int argc, char **argv) {
-  std::cout << "Hello world!\n" << std::endl;
-
   mlir::DialectRegistry registry;
 
+  registry.insert<mlir::arith::ArithDialect, mlir::func::FuncDialect,
+                  mlir::math::MathDialect>();
+
+  mlir::registerAllDialects(registry);
+
   return mlir::asMainReturnCode(
-      mlir::MlirOptMain(argc, argv, "Blah", registry));
+      mlir::MlirOptMain(argc, argv, "ignota-opt", registry));
 }
